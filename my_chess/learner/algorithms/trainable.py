2265e9af51225cfdff6f7335dc93e010fdaf17ab
Mark <mzimmerman38@gatech.edu>
2024-02-16 09:51:13 -0800

    Updated dataset for multiprocessing. Included Trainer in Trainable, likely to revert.

--- my_chess/learner/algorithms/trainable.py
+++ my_chess/learner/algorithms/trainable.py
@@ -2,11 +2,20 @@ from ray.tune.trainable import Trainable as Trainabletemp
 from ray.train.torch import TorchTrainer
 from ray.tune.tune import _Config
 
+import os
+
 class Trainable(Trainabletemp):
     def getName(self):
         return self.__class__.__name__
 
 class TrainableConfig(_Config):
+    def __init__(
+            self,
+            num_cpus=None,
+            **kwargs) -> None:
+        super().__init__()
+        self.num_cpus = num_cpus if num_cpus else os.cpu_count()
+
     def to_dict(self):
         return vars(self)
     

4d1a3a08b1c96f8846e28208b5ca4ead906e5eb9
mzimm003 <69605064+mzimm003@users.noreply.github.com>
2024-06-06 11:40:45 -0700

    Update documentation.
    Describing testing module.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,10 @@
 """
-Dedicated to my dearest love, Brenda.
+Module testing by PyTest.
+
+Example:
+    Use by terminal::
+
+        $ pytest ./tests
+        $ pytest ./tests/test_algorithms.py
+        $ pytest ./tests/test_algorithms.py::test_measure_accuracy
 """

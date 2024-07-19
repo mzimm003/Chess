80169d122c1ac408a4e73d53532b18f4fc0c81fa
mzimm003 <mzimm003@gmail.com>
2024-07-19 09:40:05 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 import setuptools
 
-setuptools.setup(packages=['my_chess'])
+# setuptools.setup(packages=['my_chess'])
+setuptools.setup(
+    # ...
+    packages=setuptools.find_packages(
+        where='.',
+        include=['my_chess'],  # alternatively: `exclude=['additional*']`
+    ),
+    package_dir={"": "."}
+    # ...
+)

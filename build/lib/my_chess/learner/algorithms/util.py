f420dc64cf2be83dc1e28b559b679aeb0c0181aa
mzimm003 <69605064+mzimm003@users.noreply.github.com>
2024-07-21 22:28:46 -0600

    Prepare hugging face space.

---
+++ build/lib/my_chess/learner/algorithms/util.py
@@ -0,0 +1,40 @@
+import torch
+
+def determine_sum_dimensions(num_dims:int, batch_idx:int=-1):
+    sum_dims = torch.arange(num_dims)
+    if batch_idx != -1:
+        sum_dims = sum_dims[sum_dims != batch_idx]
+    return sum_dims
+
+def average(metric:torch.Tensor, num_dims:int, batch_idx:int=-1, mask:torch.Tensor=None):
+    sum_dims = determine_sum_dimensions(num_dims, batch_idx)
+    org_shape = metric.shape
+    if mask is None:
+        mask = torch.tensor(org_shape)[sum_dims].prod()
+    else:
+        mask = mask.sum(sum_dims.tolist())
+    metric = metric.sum(sum_dims.tolist()) / mask
+    metric = torch.nan_to_num(metric)
+    if batch_idx != -1:
+        metric = metric.sum() / org_shape[batch_idx]
+    return metric
+
+def measure_accuracy(input:torch.Tensor, target:torch.Tensor, batch_idx:int=-1) -> torch.Tensor:
+    acc = input == target
+    acc = average(acc, input.ndim, batch_idx)
+    return acc
+
+def measure_precision(input:torch.Tensor, target:torch.Tensor, batch_idx:int=-1):
+    prec_mask = input.int() == 1
+    prec = torch.zeros_like(input, dtype=bool)
+    prec[prec_mask] = input[prec_mask] == target[prec_mask]
+    prec = average(prec, input.ndim, batch_idx, prec_mask)
+    return prec
+
+def measure_recall(input:torch.Tensor, target:torch.Tensor, batch_idx:int=-1):
+    
+    rec_mask = target.int() == 1
+    rec = torch.zeros_like(input, dtype=bool)
+    rec[rec_mask] = input[rec_mask] == target[rec_mask]
+    rec = average(rec, input.ndim, batch_idx, rec_mask)
+    return rec
---
+++ build/lib/my_chess/learner/models/__init__.py
@@ -0,0 +1,30 @@
+"""Models to learn optimal actions based on environment states"""
+
+from .model import (
+    ModelRLLIB,
+    ModelRRLIBConfig,
+    Model,
+    ModelConfig,
+    ModelAutoEncodable)
+from .tobenamed import ToBeNamed, ToBeNamedConfig
+from .Qlearner import QLearner, QLearnerConfig
+from .deepchess import (
+        DeepChessFE,
+        DeepChessFEConfig,
+        DeepChessEvaluator,
+        DeepChessEvaluatorConfig,
+        DeepChessAlphaBeta,
+        DeepChessAlphaBetaConfig,
+        DeepChessRL,
+        DeepChessRLConfig,)
+from .transformerchess import(
+        TransformerChessFE,
+        TransformerChessFEConfig
+)
+
+import inspect
+import sys
+from ray.rllib.models import ModelCatalog
+reg_models = {k:v for k,v in inspect.getmembers(sys.modules[__name__], inspect.isclass) if not "Config" in k}
+for mod_name, mod in reg_models.items():
+        ModelCatalog.register_custom_model(mod_name, mod)
